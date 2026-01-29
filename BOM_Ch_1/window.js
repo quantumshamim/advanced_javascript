cad1936e065311c15da7a7408aaa998cd5f9db48
quantumshamim <msamim7617@gmail.com>
2026-01-29 17:00:58 +0600

    window

--- BOM_Ch_1/window.js
+++ BOM_Ch_1/window.js
@@ -3,7 +3,7 @@ const closeWindow = document.getElementById("closeWindow")
 let myWindow;
 
 openNewWindow.addEventListener("click", ()=>{
-    myWindow = window.open("https://www.facebook.com/samim.767","fb")
+    myWindow = window.open("https://www.facebook.com/samim.767","fb","width=500, height=300")
 })
 
 closeWindow.addEventListener("click", ()=>{
