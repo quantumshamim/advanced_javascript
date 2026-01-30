b8ef255335192a47518b29ddd3d3c3815c0302b0
quantumshamim <msamim7617@gmail.com>
2026-01-30 17:10:16 +0600

    confirm

--- BOM_Ch_1/alert&confirm.js
+++ BOM_Ch_1/alert&confirm.js
@@ -1,1 +1,16 @@
 alert("Hi, whta's up")
+
+//confirm
+let dlt = document.getElementById("dlt")
+let para = document.getElementById("para")
+
+dlt.addEventListener("click", ()=>{
+    let userConf = confirm("hey, are you sure ?")
+
+    if(userConf){
+        para.textContent = "Deleted"
+    }
+    else{
+        para.textContent = "Not done"
+    }
+})
