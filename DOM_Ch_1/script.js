2e3cad5396a389d4691ddf287a14ced2a63fe0bc
quantumshamim <msamim7617@gmail.com>
2026-02-03 00:35:57 +0600

    DOM

---
+++ DOM_Ch_1/script.js
@@ -0,0 +1,11 @@
+const data = document.getElementById("why");
+const formData = document.getElementsByName("form")
+const tagName = document.getElementsByTagName("h1")
+const className = document.getElementsByClassName("para")
+const querySelector = document.querySelectorAll("*")
+
+console.log(data)
+console.log(formData)
+console.log(tagName)
+console.log(className)
+console.log(querySelector)
