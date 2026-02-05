94374946720329074030bf06aac7e1ec5549aabc
quantumshamim <msamim7617@gmail.com>
2026-02-05 23:51:38 +0600

    DOM 4

--- DOM_Ch_4/script.js
+++ DOM_Ch_4/script.js
@@ -10,5 +10,24 @@ let h1 = document.createElement("h1")
 h1.textContent = "this is h1 tag from js dom"
 info.appendChild(h1)
 
+data.after(h1)
 
 info.innerHTML = "<p>this is p tag</p>"
+
+
+
+
+//
+const app = document.querySelector("#app");
+
+let langs = ["js", "C", "C++", "Java", "Python"];
+
+let nodes = langs.map(
+    (proLang)=>{
+        let li = document.createElement("li");
+        li.textContent = proLang;
+        return li;
+    }
+)
+
+app.append(...nodes);
