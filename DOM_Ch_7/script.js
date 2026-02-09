a350a88704447a00c61b37e391db76349291ea48
quantumshamim <msamim7617@gmail.com>
2026-02-09 22:51:53 +0600

    DOM 7 part 2

--- DOM_Ch_7/script.js
+++ DOM_Ch_7/script.js
@@ -11,3 +11,12 @@ let style = getComputedStyle(box)
 console.log(width)
 console.log(height)
 console.log(style.marginLeft)
+
+
+let button = document.getElementById("click")
+
+function handleClick(){
+    alert("You have clicked me")
+}
+
+button.addEventListener("click", handleClick)
