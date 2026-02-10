27ab48beca86bffa8363e5d67c2e7e95e2ba6120
quantumshamim <msamim7617@gmail.com>
2026-02-10 14:29:19 +0600

    Page load event

--- DOM_Ch_8/script.js
+++ DOM_Ch_8/script.js
@@ -4,4 +4,25 @@ function clickNow(){
 
 
 const btn = document.getElementById("btn")
+
 btn.addEventListener("click", clickNow)
+
+
+
+//
+function loadImage(){
+    alert("Image has loaded")
+}
+
+
+const img= document.getElementById("img")
+img.addEventListener("load", loadImage)
+img.src = "https://picsum.photos/200/300"
+
+
+//
+addEventListener("beforeunload", (event)=>{
+    event.preventDefault()
+
+    event.returnValue = " "
+})
