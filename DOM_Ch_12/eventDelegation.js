c80827f6644a64b33eb79424a6ce8728b5b72ed1
quantumshamim <msamim7617@gmail.com>
2026-02-17 22:12:15 +0600

    event delegation done

--- DOM_Ch_12/eventDelegation.js
+++ DOM_Ch_12/eventDelegation.js
@@ -1,1 +1,17 @@
-const menu = document.getElementById("menu")  
+const menu = document.getElementById("menu");
+
+menu.addEventListener("click", (event)=>{
+    let target = event.target;
+
+    switch(target.id){
+        case "home":
+            console.log("clicked home");
+            break;
+        case "about":
+            console.log("clicked about");
+            break;
+        case "report":
+            console.log("clicked report");
+            break;
+    }
+})
