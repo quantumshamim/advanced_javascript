cf90af045a0a54653572252686432365fa9b256f
quantumshamim <msamim7617@gmail.com>
2026-02-18 16:55:05 +0600

    bom

---
+++ BOM_Ch_1/window.js
@@ -0,0 +1,11 @@
+const openNewWindow = document.getElementById("openNewWindow")
+const closeWindow = document.getElementById("closeWindow")
+let myWindow;
+
+openNewWindow.addEventListener("click", ()=>{
+    myWindow = window.open("https://www.facebook.com/samim.767","fb")
+})
+
+closeWindow.addEventListener("click", ()=>{
+    myWindow.close()
+})
