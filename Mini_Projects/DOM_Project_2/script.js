eb1c439ca62e3a86fcee13e4368ae89abae668a7
quantumshamim <msamim7617@gmail.com>
2026-02-18 13:04:06 +0600

    project 2

---
+++ Mini_Projects/DOM_Project_2/script.js
@@ -0,0 +1,16 @@
+const password = document.querySelector("#password")
+const togglePassword = document.querySelector("#togglePassword")
+
+togglePassword.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const type = password.getAttribute("type");
+
+    if(type === "password"){
+        password.setAttribute("type", "text");
+        togglePassword.textContent = "hide";
+    }
+    else{
+        password.setAttribute("type", "password");
+        togglePassword.textContent = "show"
+    }
+})
